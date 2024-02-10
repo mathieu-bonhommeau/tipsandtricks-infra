ff902bd7323ddeacbda4a35ea7babdf1acf9224d
Mathieu Bonhommeau <mat.bonhommeau85@gmail.com>
2024-02-10 19:52:09 +0100

    feat: deploy

--- docker/nginx/init.sh
+++ docker/nginx/init.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-mkdir /etc/nginx/sites-enabled
-ln -s /etc/nginx/sites-available/node_server.conf /etc/nginx/sites-enabled/node_server.conf
-ln -s /etc/nginx/sites-available/webapp.conf /etc/nginx/sites-enabled/webapp.conf
+mkdir -p /etc/nginx/sites-enabled
+
+# Copier les fichiers de configuration directement dans le dossier sites-enabled
+cp /etc/nginx/sites-available/node_server.conf /etc/nginx/sites-enabled/node_server.conf
+cp /etc/nginx/sites-available/webapp.conf /etc/nginx/sites-enabled/webapp.conf
 
 nginx -g 'daemon off;'
+
